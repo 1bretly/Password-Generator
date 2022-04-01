f6a5a7f859fcd3efc66d75f4d1acd69395d48448
Bret Andersen <1bretly@gmail.com>
2022-04-01 18:57:05 -0500

    finalizing my work

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,18 @@
-// Assignment Code & event listener
+// Assignment Code 
+// Button & event listener
 let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 // Arrays
-let PasswordLength = "";
 let alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
+let specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "?", "/"];
 
-// Function writePassword is called
+// Empty string for let passwordLengths
+let passwordLength = "";
+
+// Function writePassword is ran when button is clicked
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
@@ -17,17 +20,23 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Function generatePassword is called
+// Function generatePassword is ran
 function generatePassword() {
-  // Created a console.log for button
-  console.log("You clicked the button!");
 
   // Define passwordLength with prompt
   let passwordLength = prompt("How many characters would you like your password to be? Between 8-128");
+  if (passwordLength === null) {
+    return "";
+  }
 
   while (passwordLength <= 7 || passwordLength >= 129) {
     alert("Your password length must be between 8-128 characters!");
+
     passwordLength = prompt("How many characters would you like your password to be? Between 8-128");
+
+    if (passwordLength === null) {
+      return "";
+    }
   }
   // Confirmation on password length
   alert(`Your password will have ${passwordLength} characters`);
@@ -38,11 +47,12 @@ function generatePassword() {
   let confirmationNumber = confirm("Click OK if you would like to include numbers?");
   let confirmationSpecial = confirm("Click OK if you would like to include special characters?");
   while (confirmationLower === false && confirmationUpper === false && confirmationNumber === false && confirmationSpecial === false) {
-    alert("Hey IDIOT, you must choose at least one parameter!");
-    let confirmationLower = confirm("Click OK if you would like to include lowercase characters?");
-    let confirmationUpper = confirm("Click OK if you would like to include uppercase characters?");
-    let confirmationNumbers = confirm("Click OK if you would like to include numbers?");
-    let confirmationSpecial = confirm("Click OK if you would like to include special characters?");
+    alert("Choose at least one parameter!");
+
+    confirmationLower = confirm("Click OK if you would like to include lowercase characters?");
+    confirmationUpper = confirm("Click OK if you would like to include uppercase characters?");
+    confirmationNumbers = confirm("Click OK if you would like to include numbers?");
+    confirmationSpecial = confirm("Click OK if you would like to include special characters?");
   }
 
   let passwordCharacters = []
