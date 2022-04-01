fce004e0d39bfc4ea8ccefd2a8700bdf50d4f8e5
Bret Andersen <1bretly@gmail.com>
2022-03-31 22:26:49 -0500

    got generator working

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
+// Assignment Code & event listener
+let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-// Strings
-let alphaLow = "abcdefghijklmnopqrstuvwxyz";
-let alphaHigh = "ABCDERFGHIJKLMNOPQRSTUVWXYZ";
-let numbers = "0123456789";
-let symbols = "!@#$%^&*";
+// Arrays
+let PasswordLength = "";
+let alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 
-// function writePassword is called
+// Function writePassword is called
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// Cunction generatePassword is called
+// Function generatePassword is called
 function generatePassword() {
   // Created a console.log for button
   console.log("You clicked the button!");
@@ -26,7 +25,7 @@ function generatePassword() {
   // Define passwordLength with prompt
   let passwordLength = (prompt("How many characters would you like your password to be? Between 8-128"));
 
-  while (passwordLength <= 7 || passwordLength >= 128) {
+  while (passwordLength <= 7 || passwordLength >= 129) {
     alert("Your password length must be between 8-128 characters!");
     let passwordLength = (prompt("How many characters would you like your password to be? Between 8-128"));
   }
@@ -36,28 +35,42 @@ function generatePassword() {
   // Determine password parameters. (lowercase, uppercase, #s, special)
   let confirmationLower = confirm("Click OK if you would like to include lowercase characters?");
   let confirmationUpper = confirm("Click OK if you would like to include uppercase characters?");
-  let confirmationNumbers = confirm("Click OK if you would like to include numbers?");
+  let confirmationNumber = confirm("Click OK if you would like to include numbers?");
   let confirmationSpecial = confirm("Click OK if you would like to include special characters?");
-  while (confirmationLower === false && confirmationUpper === false && confirmationNumbers === false && confirmationSpecial === false) {
+  while (confirmationLower === false && confirmationUpper === false && confirmationNumber === false && confirmationSpecial === false) {
     alert("Hey IDIOT, you must choose at least one parameter!");
+    let confirmationLower = confirm("Click OK if you would like to include lowercase characters?");
+    let confirmationUpper = confirm("Click OK if you would like to include uppercase characters?");
+    let confirmationNumbers = confirm("Click OK if you would like to include numbers?");
+    let confirmationSpecial = confirm("Click OK if you would like to include special characters?");
   }
 
-  // 2) Validate the input
+  let passwordCharacters = []
 
-  // 3) Display the generated password on the page
+  if (confirmationLower) {
+    passwordCharacters = passwordCharacters.concat(alphaLower)
+  }
 
-  return "Generated password will go here!"
+  if (confirmationUpper) {
+    passwordCharacters = passwordCharacters.concat(alphaUpper)
+  }
+
+  if (confirmationNumber) {
+    passwordCharacters = passwordCharacters.concat(number)
+  }
+
+  if (confirmationSpecial) {
+    passwordCharacters = passwordCharacters.concat(specialCharacters)
+  }
+
+  console.log(passwordCharacters);
+
+  // Empty string that will be filled by for loop outcome
+  let randomPassword = ""
+
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
+  }
+  return randomPassword
 }
-
-
-
-// let passwordLength = 0;
-// while (passwordLength < 8 || passwordLength > 128) {
-//   passwordLength = window.prompt("How many characters would you like your password to be?");
-//   console.log(passwordLength)
-//   if g
-
-
-
-
-
